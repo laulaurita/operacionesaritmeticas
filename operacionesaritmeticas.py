deaaafee96c23f0d8f5717bb566a753e42d753b2
laulaurita <laurasuarez@colegioguanenta.edu.co>
2024-02-22 16:58:02 -0500

    se realizo el diseño, analisis y programacion

---
+++ operacionesaritmeticas.py
@@ -0,0 +1,16 @@
+# input
+x= int(input("digite el valor de x:"))
+y= int(input("digite el valor de y:"))   
+
+ #pressing
+S= x+y
+r= x-y
+m= x*y
+d= x/y
+de= x//y
+mod= x%y
+p= x**y
+
+ #output
+print("Resultados")
+print("la suma de"+str(x)+"+"+str(y)+"es"+str(S))
